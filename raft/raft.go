99217a676aa126f63528ac57cc115c3481296759
sbruckner-tgm <sbruckner@student.tgm.ac.at>
2020-11-30 16:28:27 +0100

    raft initialize node almost complete

--- raft/raft.go
+++ raft/raft.go
@@ -6,6 +6,8 @@ import (
 	bolt "github.com/hashicorp/raft-boltdb"
 	"log"
 	"net"
+	"net/http"
+	"net/url"
 	"os"
 	"path/filepath"
 	"time"
@@ -18,8 +20,58 @@ func main(){
 		fmt.Fprintf(os.Stderr, "Configuration errors - %s\n", err)
 		os.Exit(1)
 	}
+	raftLogger := log.New(os.Stdout,"raft",log.Ltime)
+	raftNode, err := NewNode(config, raftLogger)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error configuring node: %s", err)
+		os.Exit(1)
+	}
 
-	raftNode, error := NewNode(config)
+	if config.JoinAddress != "" {
+		go func() {
+			retryJoin := func() error {
+				joinUrl := url.URL{
+					Scheme: "http",
+					Host:   config.JoinAddress,
+					Path:   "join",
+				}
+
+				req, err := http.NewRequest(http.MethodPost, joinUrl.String(), nil)
+				if err != nil {
+					return err
+				}
+				req.Header.Add("Peer-Address", config.RaftAddress.String())
+
+				resp, err := http.DefaultClient.Do(req)
+				if err != nil {
+					return err
+				}
+
+				if resp.StatusCode != http.StatusOK {
+					return fmt.Errorf("non 200 status code: %d", resp.StatusCode)
+				}
+
+				return nil
+			}
+
+			for {
+				if err := retryJoin(); err != nil {
+					//logger.Error().Err(err).Str("component", "join").Msg("Error joining cluster")
+					fmt.Println("Error joining cluster")
+					time.Sleep(1 * time.Second)
+				} else {
+					break
+				}
+			}
+		}()
+	}
+	httpLogger := log.New(os.Stdout,"http",log.Ltime)
+	service := &service{
+		node: raftNode,
+		address: config.HTTPAddress,
+		logger: &httpLogger,
+	}
+	service.Start()
 }
 
 type node struct {
@@ -28,7 +80,7 @@ type node struct {
 	fsm      *fsm
 	log      *log.Logger
 }
-func NewNode(config *Config) (*node, error){
+func NewNode(config *Config, log *log.Logger) (*node, error){
 
 	raftConfig := raft.DefaultConfig()
 
@@ -44,15 +96,16 @@ func NewNode(config *Config) (*node, error){
 	if err != nil {
 		return nil, err
 	}
-	snapshotStore, err := raft.NewFileSnapshotStore(config.DataDir,1,)
+	snapshotStoreLogger := log.Writer()
+	snapshotStore, err := raft.NewFileSnapshotStore(config.DataDir,1,snapshotStoreLogger)
 	if err != nil {
 		return nil, err
 	}
-	transport, err := newTransport(config)
+	transport, err := newTransport(config, log)
 	if err!= nil {
 		return nil, err
 	}
-	raftNode, _ := raft.NewRaft(raftConfig,fsm, logStore, stableStore, snapshotStore, transport)
+	raftNode, err := raft.NewRaft(raftConfig,fsm, logStore, stableStore, snapshotStore, transport)
 	if err != nil {
 		return nil, err
 	}
@@ -74,12 +127,12 @@ func NewNode(config *Config) (*node, error){
 		fsm:      fsm,
 	}, nil
 }
-func newTransport(config *Config) (*raft.NetworkTransport, error){
+func newTransport(config *Config, logger *log.Logger) (*raft.NetworkTransport, error){
 	address, err := net.ResolveTCPAddr("tcp",config.RaftAddress.String())
 	if err != nil {
 		return nil, err
 	}
-	transport, err := raft.NewTCPTransport(address.String(), config.HTTPAddress, 3, 10*time.Second, log)
+	transport, err := raft.NewTCPTransport(address.String(), config.HTTPAddress, 3, 10*time.Second, logger)
 	if err != nil {
 		return nil, err
 	}
