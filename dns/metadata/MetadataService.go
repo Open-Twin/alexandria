94f4b53f258e74fd7935ea0d68ebc4095d3ea694
Dejan Plisnic <dplisnic@student.tgm.ac.at>
2020-12-14 17:41:32 +0100

    Worked on the Service

--- dns/metadata/MetadataService.go
+++ dns/metadata/MetadataService.go
@@ -2,6 +2,7 @@ package metadata
 
 import (
 	""
+	"errors"
 )
 
 /*
@@ -13,30 +14,44 @@ type repository = MetadataRepository
 type requestType = string
 
 type MetadataService interface {
-	Create(repository MetadataRepository) MetadataServiceImpl
+	Init(repository MetadataRepository) MetadataServiceImpl
 	HandleRequest(service, ip, key, requestType string) error
 	POSTRequest(requestType string) bool
 	GETRequest(requestType string) bool
+	ResponseRequest()
 }
 
 type MetadataServiceImpl struct {
 	repository MetadataRepository
 }
 
-func Create(repository MetadataRepository) MetadataServiceImpl {
+func Init(repository MetadataRepository) MetadataServiceImpl {
 	return MetadataServiceImpl{
 		repository: repository,
 	}
 }
 
-func (m *MetadataServiceImpl) ... {
-	if m.repository.Exists(key) {
-		...
-	} else {
-		err := m.repository.Create(...)
+func POSTRequest(requestType string) bool {
+	...
+}
+
+func GETRequest(requestType string) bool {
+	...
+}
+
+func ResponseRequest() {
+	...
+}
+
+func (m *MetadataServiceImpl) HandleRequest (service, ip, key, value, requestType string) error {
+	if !m.repository.Exists(service, ip, key) {
+		if requestType == "get" {
+			return errors.New("not existing: the given key doesnt exist, thus no data can be returned")
+		}
+		err := m.repository.Create(service, ip, key, value)
 		if err != nil {
 			panic(err)
 		}
-		...
+
 	}
 }
