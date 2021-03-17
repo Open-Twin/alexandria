5acef1017fc947cc20b06423edfd27e0105a226a
Sebastian Bruckner-Hrubesch <sbruckner@student.tgm.ac.at>
2021-03-17 23:09:40 +0100

    fixed dns test addr

--- dns/dns_test.go
+++ dns/dns_test.go
@@ -16,12 +16,12 @@ import (
 func TestMain(m *testing.M) {
 	logger := log.New(os.Stdout,"",log.Ltime)
 
-	raftaddr := &net.TCPAddr{
+	raftaddr := net.TCPAddr{
 		IP: net.ParseIP("127.0.0.1"),
 		Port: 7000,
 	}
 
-	httpaddr := &net.TCPAddr{
+	httpaddr := net.TCPAddr{
 		IP: net.ParseIP("127.0.0.1"),
 		Port: 8000,
 	}
